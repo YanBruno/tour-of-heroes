a01a656891400e6f416b989c375254ba11ffd7e9
Yan Santos <yanbrunosilvasantos@gmail.com>
2022-10-20 06:41:44 -0300

    correcao imports

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, first, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
