7c1e1cf05924fc14064e3c61fc1c421d456241b4
Yan Santos <yanbrunosilvasantos@gmail.com>
2022-10-22 12:02:41 -0300

    correcao imports

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../auth/guards/auth.guard';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [{ path: '', component: DashboardComponent }];
